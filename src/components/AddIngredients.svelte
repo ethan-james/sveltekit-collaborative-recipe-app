718b90c3264214d5bf565d726fe7bf9f181a39fa
Ethan James <ethan.e.james@gmail.com>
2024-10-29 15:25:16 -0700

    addIngredientToShoppingList

---
+++ src/components/AddIngredients.svelte
@@ -0,0 +1,22 @@
+<form method="POST" action="?/insert">
+	<fieldset>
+		<textarea
+			id="ingredients"
+			name="ingredients"
+			placeholder="Ingredients (comma-separated or one per line)"
+		/>
+		<slot />
+		<button>Add Ingredients</button>
+	</fieldset>
+</form>
+
+<style>
+	button {
+		@apply btn btn-primary;
+	}
+
+	textarea {
+		height: 10rem;
+		@apply textarea textarea-bordered;
+	}
+</style>
