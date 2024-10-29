33bd2e604e709239a21324e4707a31ea6306c0aa
Ethan James <ethan.e.james@gmail.com>
2024-10-29 15:26:33 -0700

    recipe ingredient list

--- src/routes/recipe/[id]/+page.svelte
+++ src/routes/recipe/[id]/+page.svelte
@@ -1,66 +1,159 @@
 <script>
 	import { DateInput } from "date-picker-svelte";
 	import { tick } from "svelte";
-	import { recipes } from "../../../db/schema";
+	import Fa from "svelte-fa";
+	import { faMinusSquare, faPlusSquare, faXmarkCircle } from "@fortawesome/free-regular-svg-icons";
+	import { faLock, faLockOpen } from "@fortawesome/free-solid-svg-icons";
+	import { Status } from "../../../db/ingredients";
+	import AddIngredients from "../../../components/AddIngredients.svelte";
 
 	export let data;
-	let date = data.recipe?.scheduled && new Date(data.recipe?.scheduled);
+	let date = data.meal?.scheduled ? new Date(data.meal?.scheduled) : null;
+	/**
+	 * @type {HTMLFormElement}
+	 */
 	let form;
 
+	const formatter = new Intl.DateTimeFormat("fr-CA");
+
+	const getStatusIcon = (/** @type {Status} */ status) => {
+		switch (status) {
+			case Status.None:
+				return "•";
+			case Status.Missing:
+				return "☐";
+			case Status.Completed:
+				return "✓";
+		}
+	};
+
 	$: date &&
-		date.toISOString().slice(0, 10) !== data.recipe?.scheduled &&
+		date.toISOString().slice(0, 10) !== data.meal?.scheduled &&
 		tick().then(() => form?.requestSubmit());
-	// $: date =
 </script>
 
-<header>
-	<form method="POST" action="?/schedule" bind:this={form}>
-		<DateInput bind:value={date} closeOnSelection format="MM/dd" placeholder="Schedule Meal" />
-		<input type="hidden" name="id" value={data.recipe?.id} />
-		<input type="hidden" name="scheduled" value={date} />
-	</form>
-	<form method="POST" action="?/schedule">
-		<input type="hidden" name="id" value={data.recipe?.id} />
-		<input type="hidden" name="scheduled" value={null} />
-		<button>✗</button>
-	</form>
-	<h3>{data.recipe?.name}</h3>
-	<a href={data.recipe?.url} title="View →" target="_blank">View →</a>
-</header>
+<main>
+	<section>
+		<header>
+			<form method="POST" action="?/schedule">
+				<input type="hidden" name="id" value={data.recipe?.id} />
+				<input type="hidden" name="scheduled" value={null} />
+				<button>✗</button>
+			</form>
+			<form method="POST" action="?/schedule" bind:this={form}>
+				<DateInput bind:value={date} closeOnSelection format="MM/dd" placeholder="Schedule Meal" />
+				<input type="hidden" name="id" value={data.recipe?.id} />
+				<input type="hidden" name="scheduled" value={date ? formatter.format(date) : null} />
+			</form>
+			<h3>{data.recipe?.name}</h3>
+		</header>
+		<aside>
+			<a href={data.recipe?.url} title="View →" target="_blank">View →</a>
+		</aside>
+		{#if data.ingredients.length}
+			<footer>
+				<ul>
+					{#each data.ingredients as { id, locked, name, status }}
+						<li>
+							<span>{getStatusIcon(status)} {name}</span>
+							<div class="btn-row">
+								{#if !locked}
+									{#if status == Status.None}
+										<form method="POST" action="?/addIngredientToShoppingList">
+											<input type="hidden" name="ingredientId" value={id} />
+											<input type="hidden" name="listId" value="1" />
+											<input type="hidden" name="recipeId" value={data.recipe?.id} />
+											<button>
+												<Fa class="cursor-pointer text-green-600" icon={faPlusSquare} />
+											</button>
+										</form>
+									{:else}
+										<form method="POST" action="?/removeIngredientFromShoppingList">
+											<input type="hidden" name="ingredientId" value={id} />
+											<input type="hidden" name="listId" value="1" />
+											<input type="hidden" name="recipeId" value={data.recipe?.id} />
+											<button>
+												<Fa class="cursor-pointer text-remove-600" icon={faMinusSquare} />
+											</button>
+										</form>
+									{/if}
+								{/if}
+								{#if locked}
+									<form method="POST" action="?/unlockIngredient">
+										<input type="hidden" name="ingredientId" value={id} />
+										<button>
+											<Fa class="cursor-pointer text-blue-600" icon={faLock} />
+										</button>
+									</form>
+								{:else}
+									<form method="POST" action="?/lockIngredient">
+										<input type="hidden" name="ingredientId" value={id} />
+										<button>
+											<Fa class="cursor-pointer text-slate-200" icon={faLockOpen} />
+										</button>
+									</form>
+								{/if}
+								<form method="POST" action="?/removeIngredientFromRecipe">
+									<input type="hidden" name="ingredientId" value={id} />
+									<input type="hidden" name="recipeId" value={data.recipe?.id} />
+									<button>
+										<Fa class="cursor-pointer text-red-600" icon={faXmarkCircle} />
+									</button>
+								</form>
+							</div>
+						</li>
+					{/each}
+				</ul>
+			</footer>
+		{/if}
+	</section>
+</main>
 
-<ul>
-	{#each data.ingredients as ingredient}
-		<li>{ingredient.name}</li>
-	{/each}
-</ul>
+{#if data.recipe}
+	<AddIngredients>
+		<input type="hidden" name="recipeId" value={data.recipe.id} />
+	</AddIngredients>
+{/if}
 
-<form method="POST" action="?/insert">
-	<label for="ingredients">Ingredients (comma-separated or one per line)</label>
-	<textarea id="ingredients" name="ingredients" />
-	<input type="hidden" name="recipeId" value={data.recipe?.id} />
-	<button>Add Ingredients</button>
-</form>
-
-<style>
+<style lang="postcss">
 	:global(header .date-time-field input) {
 		border: 0;
 		display: inline;
 	}
 
+	main {
+		@apply card card-bordered;
+	}
+
+	section {
+		@apply card-body;
+	}
+
 	header {
-		display: flex;
-		align-items: center;
-		gap: 1rem;
-		margin-left: 1rem;
+		@apply border-gray-200 card-title;
 	}
 
-	form {
-		display: flex;
-		flex-direction: column;
-		gap: 1rem;
+	aside {
+		@apply bg-gray-200 flex justify-end p-2 rounded-lg -translate-y-2;
 	}
 
-	textarea {
-		height: 10rem;
+	footer {
+		@apply card-body;
+	}
+
+	ul {
+		@apply border-t border-gray-200;
+	}
+
+	ul li {
+		@apply border-b border-gray-200 flex items-center justify-between list-none px-2 py-1;
+	}
+
+	ul li form {
+		@apply flex items-center;
+	}
+
+	.btn-row {
+		@apply flex gap-4 whitespace-nowrap;
 	}
 </style>
