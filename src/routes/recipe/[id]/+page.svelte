2da46a717c4b48386498105eeafee10b44dc6ff3
Ethan James <ethan.e.james@gmail.com>
2024-11-01 11:59:27 -0700

    add card component from shadcn

--- src/routes/recipe/[id]/+page.svelte
+++ src/routes/recipe/[id]/+page.svelte
@@ -6,6 +6,7 @@
 	import Fa from "svelte-fa";
 	import { faMinusSquare, faPlusSquare, faXmarkCircle } from "@fortawesome/free-regular-svg-icons";
 	import { faLock, faLockOpen } from "@fortawesome/free-solid-svg-icons";
+	import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
 	import { Status } from "../../../db/ingredients";
 	import AddIngredients from "../../../components/AddIngredients.svelte";
 
@@ -36,82 +37,80 @@
 	});
 </script>
 
-<main>
-	<section>
-		<header>
-			<form method="POST" action="?/schedule">
-				<input type="hidden" name="id" value={data.recipe?.id} />
-				<input type="hidden" name="scheduled" value={null} />
-				<button>✗</button>
-			</form>
-			<form method="POST" action="?/schedule" bind:this={form}>
-				<DateInput bind:value={date} closeOnSelection format="MM/dd" placeholder="Schedule Meal" />
-				<input type="hidden" name="id" value={data.recipe?.id} />
-				<input type="hidden" name="scheduled" value={date ? formatter.format(date) : null} />
-			</form>
-			<h3>{data.recipe?.name}</h3>
-		</header>
-		<aside>
-			<a href={data.recipe?.url} title="View →" target="_blank">View →</a>
-		</aside>
-		{#if data.ingredients.length}
-			<footer>
-				<ul>
-					{#each data.ingredients as { id, locked, name, status }}
-						<li>
-							<span>{getStatusIcon(status)} {name}</span>
-							<div class="btn-row">
-								{#if !locked}
-									{#if status == Status.None}
-										<form method="POST" action="?/addIngredientToShoppingList">
-											<input type="hidden" name="ingredientId" value={id} />
-											<input type="hidden" name="listId" value="1" />
-											<input type="hidden" name="recipeId" value={data.recipe?.id} />
-											<button>
-												<Fa class="cursor-pointer text-green-600" icon={faPlusSquare} />
-											</button>
-										</form>
-									{:else}
-										<form method="POST" action="?/removeIngredientFromShoppingList">
-											<input type="hidden" name="ingredientId" value={id} />
-											<input type="hidden" name="listId" value="1" />
-											<input type="hidden" name="recipeId" value={data.recipe?.id} />
-											<button>
-												<Fa class="cursor-pointer text-remove-600" icon={faMinusSquare} />
-											</button>
-										</form>
-									{/if}
-								{/if}
-								{#if locked}
-									<form method="POST" action="?/unlockIngredient">
+<Card>
+	<CardHeader>
+		<form method="POST" action="?/schedule">
+			<input type="hidden" name="id" value={data.recipe?.id} />
+			<input type="hidden" name="scheduled" value={null} />
+			<button>✗</button>
+		</form>
+		<form method="POST" action="?/schedule" bind:this={form}>
+			<DateInput bind:value={date} closeOnSelection format="MM/dd" placeholder="Schedule Meal" />
+			<input type="hidden" name="id" value={data.recipe?.id} />
+			<input type="hidden" name="scheduled" value={date ? formatter.format(date) : null} />
+		</form>
+		<h3>{data.recipe?.name}</h3>
+	</CardHeader>
+	<CardContent>
+		<a href={data.recipe?.url} title="View →" target="_blank">View →</a>
+	</CardContent>
+	{#if data.ingredients.length}
+		<CardFooter>
+			<ul>
+				{#each data.ingredients as { id, locked, name, status }}
+					<li>
+						<span>{getStatusIcon(status)} {name}</span>
+						<div class="btn-row">
+							{#if !locked}
+								{#if status == Status.None}
+									<form method="POST" action="?/addIngredientToShoppingList">
 										<input type="hidden" name="ingredientId" value={id} />
+										<input type="hidden" name="listId" value="1" />
+										<input type="hidden" name="recipeId" value={data.recipe?.id} />
 										<button>
-											<Fa class="cursor-pointer text-blue-600" icon={faLock} />
+											<Fa class="cursor-pointer text-green-600" icon={faPlusSquare} />
 										</button>
 									</form>
 								{:else}
-									<form method="POST" action="?/lockIngredient">
+									<form method="POST" action="?/removeIngredientFromShoppingList">
 										<input type="hidden" name="ingredientId" value={id} />
+										<input type="hidden" name="listId" value="1" />
+										<input type="hidden" name="recipeId" value={data.recipe?.id} />
 										<button>
-											<Fa class="cursor-pointer text-slate-200" icon={faLockOpen} />
+											<Fa class="cursor-pointer text-remove-600" icon={faMinusSquare} />
 										</button>
 									</form>
 								{/if}
-								<form method="POST" action="?/removeIngredientFromRecipe">
+							{/if}
+							{#if locked}
+								<form method="POST" action="?/unlockIngredient">
 									<input type="hidden" name="ingredientId" value={id} />
-									<input type="hidden" name="recipeId" value={data.recipe?.id} />
 									<button>
-										<Fa class="cursor-pointer text-red-600" icon={faXmarkCircle} />
+										<Fa class="cursor-pointer text-blue-600" icon={faLock} />
 									</button>
 								</form>
-							</div>
-						</li>
-					{/each}
-				</ul>
-			</footer>
-		{/if}
-	</section>
-</main>
+							{:else}
+								<form method="POST" action="?/lockIngredient">
+									<input type="hidden" name="ingredientId" value={id} />
+									<button>
+										<Fa class="cursor-pointer text-slate-200" icon={faLockOpen} />
+									</button>
+								</form>
+							{/if}
+							<form method="POST" action="?/removeIngredientFromRecipe">
+								<input type="hidden" name="ingredientId" value={id} />
+								<input type="hidden" name="recipeId" value={data.recipe?.id} />
+								<button>
+									<Fa class="cursor-pointer text-red-600" icon={faXmarkCircle} />
+								</button>
+							</form>
+						</div>
+					</li>
+				{/each}
+			</ul>
+		</CardFooter>
+	{/if}
+</Card>
 
 {#if data.recipe}
 	<AddIngredients>
@@ -125,24 +124,8 @@
 		display: inline;
 	}
 
-	main {
-		@apply card card-bordered;
-	}
-
-	section {
-		@apply card-body;
-	}
-
-	header {
-		@apply border-gray-200 card-title;
-	}
-
 	aside {
 		@apply bg-gray-200 flex justify-end p-2 rounded-lg -translate-y-2;
-	}
-
-	footer {
-		@apply card-body;
 	}
 
 	ul {
