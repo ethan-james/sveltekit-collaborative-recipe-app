fb77859eff050dd66679656b3372c6a42e6a37da
Ethan James <ethan.e.james@gmail.com>
2025-01-17 11:58:24 -0800

    re-add @tailwindcss/postcss, $env/dynamic/private

--- src/db/adapter.ts
+++ src/db/adapter.ts
@@ -1,21 +1,12 @@
-import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
+import { env } from "$env/dynamic/private";
 
 import * as schema from "./schema";
 
-declare const process: {
-	env: {
-		TURSO_CONNECTION_URL: string;
-		TURSO_AUTH_TOKEN: string;
-	};
-};
-
-config({ path: ".env" });
-
 const client = createClient({
-	url: process.env.TURSO_CONNECTION_URL!,
-	authToken: process.env.TURSO_AUTH_TOKEN!
+	url: env.TURSO_CONNECTION_URL,
+	authToken: env.TURSO_AUTH_TOKEN
 });
 
 export const db = drizzle(client, { schema });
